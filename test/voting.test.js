5e737e1924371bdb1ae0ef3964a4b07c23a6dea8
Jewel Jo Prince <jeweljoesphprince@gmail.com>
2019-02-22 15:03:52 +0530

    test

--- test/voting.test.js
+++ test/voting.test.js
@@ -13,7 +13,7 @@ beforeEach(async () => {
  
  voting = await new web3.eth.Contract(JSON.parse(interface))
    .deploy({data : bytecode })
-   .send({ from: accounts[0] , gas: '1000000' });
+   .send({ from: accounts[0] , gas: '2000000' });
 
 });
 
@@ -21,5 +21,24 @@ describe('E-voting Contract', () => {
   it('deploys a contract', () => {
     assert.ok(voting.options.address);
   });
+
+
+  it('allows one account to enter', async() => {
+    await voting.methods.addCandidate('cand1','party1').send({
+        from: accounts[0]
+    });
+    const result = await voting.methods.candidate.call({
+      from : accounts[0]
+    });
+    assert.equal (result[0],'cand1',);
+    assert.equal (result[1], 'party1');
+
+   /* const numcandidates = await voting.methods.getNumOfCandidates().call({
+      from: accounts[0]
+    });
+    assert.equal(accounts[0],candidate[0]);
+    assert.equal(1,numcandidates);*/
+});
+
   
 });
